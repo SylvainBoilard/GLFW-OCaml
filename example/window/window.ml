cb7ea3aaebee76d957e03e059a8d486322911701
Sylvain Boilard <boilard@crans.org>
2019-06-12 20:14:43 +0200

    Added comments to window example.

--- example/window/window.ml
+++ example/window/window.ml
@@ -1,12 +1,19 @@
 external glClearColorBufferBit : unit -> unit = "caml_glClearColorBufferBit"
 
 let () =
+  (* Initialize the library *)
   GLFW.init ();
   at_exit GLFW.terminate;
+  (* Create a windowed mode window and its OpenGL context *)
   let window = GLFW.createWindow 640 480 "Hello World" () in
+  (* Make the window's context current *)
   GLFW.makeContextCurrent (Some window);
+  (* Loop until the user closes the window *)
   while not (GLFW.windowShouldClose window) do
+    (* Render here *)
     glClearColorBufferBit ();
+    (* Swap front and back buffers *)
     GLFW.swapBuffers window;
+    (* Poll for and process events *)
     GLFW.pollEvents ()
   done
